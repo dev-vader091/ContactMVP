f974fa1777beb24dedae2de21b5f587728515932
dev-vader091 <86165493+dev-vader091@users.noreply.github.com>
2023-01-31 17:07:16 -0600

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContactMVP.Data;
+using ContactMVP.Models;
+using ContactMVP.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ContactMVP.Controllers
+{
+    [Authorize]
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+
+        public ContactsController(ApplicationDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Contacts
+        
+        public async Task<IActionResult> Index()
+        {
+            string userId = _userManager.GetUserId(User)!;
+
+            List<Contact> contacts = new List<Contact>();
+
+            contacts = await  _context.Contacts.Where(c => c.AppUserId == userId).ToListAsync(); 
+
+            return View(contacts);
+        }
+
+        // GET: Contacts/Details/5
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .Include(c => c.AppUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+      
+        public IActionResult Create()
+        {
+            //ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
+
+            ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(States)).Cast<States>());
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public async Task<IActionResult> Create([Bind("Id,AppUserId,FirstName,LastName,BirthDate,Address1,Address2,City,State,ZipCode,Email,Phone,Created,ImageData,ImageType")] Contact contact)
+        {
+
+            ModelState.Remove("AppUserId");
+
+            if (ModelState.IsValid)
+            {
+                contact.AppUserId = _userManager.GetUserId(User);
+
+                if (contact.BirthDate != null)
+                {
+                    contact.BirthDate = DateTime.SpecifyKind(contact.BirthDate.Value, DateTimeKind.Utc);
+                }
+
+                contact.Created = DateTime.UtcNow;
+
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(States)).Cast<States>());
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(States)).Cast<States>());
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AppUserId,FirstName,LastName,BirthDate,Address1,Address2,City,State,ZipCode,Email,Phone,Created,ImageData,ImageType")] Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactExists(contact.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(States)).Cast<States>());
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Contacts == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .Include(c => c.AppUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Contacts == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Contacts'  is null.");
+            }
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                _context.Contacts.Remove(contact);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactExists(int id)
+        {
+          return (_context.Contacts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactMVP.Models
+{
+    public class AppUser : IdentityUser 
+    {
+        // [Required --] allows for code to continue running w/null values
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        // [NotMapped] -- makes it so property not tracked in DB
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        // Navigation Properties
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+
+
+
+    }
+}
